7fecd6305cfe130215d2995148c706306d53ed91
Wouter Houweling <whouweling@gmail.com>
2016-01-24 13:53:46 +0100

    Added hashbang so python3 is used by default

--- yelena.py
+++ yelena.py
@@ -1,24 +1,29 @@
-import binascii
+#!/usr/bin/env python3
+
+VERSION = "0.3"
+
 import sys
+
+try:
+    import settings
+except ImportError:
+    print("Error: missing 'settings.py'")
+    sys.exit(1)
 
 from lib.context import Context
 from lib.core import Core
 from lib.webserver import Webserver
 
-from datetime import datetime
-
 import logging
 
+if __name__ == '__main__':
 
-import settings
+    logging.info("Yelena, version {version} starting ...".format(version=VERSION))
 
+    context = Context()
+    core = Core(context=context)
+    webserver = Webserver(core=core,
+                          context=context)
+    webserver.start()
 
-context = Context()
-
-core = Core(context=context)
-
-webserver = Webserver(core=core,
-                      context=context)
-webserver.start()
-
-core.start()
+    core.start()
