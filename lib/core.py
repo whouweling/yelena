a5e534f233871b086204a12f4115a588f489e826
Wouter Houweling <whouweling@gmail.com>
2016-02-13 17:38:34 +0100

    Fixed indentation

--- lib/core.py
+++ lib/core.py
@@ -15,50 +15,46 @@ import settings
 
 class Core():
 
-   def __init__(self, context):
-     super(Core, self).__init__()
-     self.context = context
-     self.alarm_switch = False
-     self.last_rule = None
-     context.update("ai", False)
+    def __init__(self, context):
+        super(Core, self).__init__()
+        self.context = context
+        self.alarm_switch = False
+        self.last_rule = None
+        context.update("ai", False)
 
-   def start(self):
-    while True:
-      if self.context.get("ai"):
-        for rule in rules:
-            if rule.check():
-                if self.last_rule != rule:
-                    rule.activate()
-                    get_logger().event("rule", "rule '%s' activated" % rule.name)
-                    self.context.update("auto-status", rule.name)
-                    self.last_rule = rule
-                break
+    def start(self):
+        while True:
+            if self.context.get("ai"):
+                for rule in rules:
+                    if rule.check():
+                        if self.last_rule != rule:
+                            rule.activate()
+                            get_logger().event("rule", "rule '%s' activated" % rule.name)
+                            self.context.update("auto-status", rule.name)
+                            self.last_rule = rule
+                        break
 
-      for device in devices:
-          device.run()
+            for device in devices:
+                device.run()
 
-      time.sleep(3)
+            time.sleep(3)
 
-   def notify(self, msg):
-      print(" *** {msg}".format(msg=msg))
+    def execute(self, action, target=None):
 
-   def execute(self, action, target=None):
-
-       if action == "switch":
+        if action == "switch":
             device = get_device(target)
             device.switch(not device.get_status())
             get_logger().event("rule", "manually switched '%s' to '%s'" % (device.name, ("on" if device.get_status() else "off")))
 
-
-       if action == "dim":
+        if action == "dim":
             device = get_device(target)
             device.dim()
 
-       if action == "undim":
-           device = get_device(target)
-           device.undim()
+        if action == "undim":
+            device = get_device(target)
+            device.undim()
 
-       if action == "toggle_ai":
-           self.context.update("ai", not self.context.get("ai"))
+        if action == "toggle_ai":
+            self.context.update("ai", not self.context.get("ai"))
 
 
